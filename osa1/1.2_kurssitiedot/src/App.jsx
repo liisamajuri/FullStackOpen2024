236af091d33f2ccbe49bb3c447e56da34d961513
Liisa Majuri <liisamajuri@kamk.fi>
2024-05-26 21:48:56 +0300

    Tehty tehtävä 1.2

---
+++ osa1/1.2_kurssitiedot/src/App.jsx
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.parts[0].part} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].part} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].part} {props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Number of exercises {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7 },
+    { part: 'State of a component', exercises: 14 },
+  ]
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+
+      <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+    </div>
+  )
+}
+
+export default App
