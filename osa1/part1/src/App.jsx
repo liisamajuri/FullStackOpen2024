1767c15218f382bab102dced84e9eb8df98f86c0
Liisa Majuri <liisamajuri@kamk.fi>
2024-05-26 16:59:31 +0300

    Tehty luentomateriaalin  part1-harjoituksia

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,15 +1,30 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+  const nimi = 'Pekka'
+  const ika = 10
+  const friends = [
+    { name: 'Leevi', age: 4 },
+    { name: 'Venla', age: 10 },
+  ]
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
     </div>
   )
 }
