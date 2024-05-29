b519afb12fe72bca29a1beb28f808c9587f2408e
Liisa Majuri <liisamajuri@kamk.fi>
2024-05-29 22:44:05 +0300

    Korjaus tehtävään 1.6

--- osa1/1.7_unicafe/src/App.jsx
+++ osa1/1.7_unicafe/src/App.jsx
@@ -9,24 +9,39 @@ const Button = ({ handleClick, text }) => (
 )
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [feedbackValues, setValues] = useState([])
+  const [total, setTotal] = useState(0)
 
   const goodClick = () => {
+    setValues(feedbackValues.concat(1))
     const updatedGood = good + 1
     setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
   }
 
   const neutralClick = () => {
+    setValues(feedbackValues.concat(0))
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
   }
 
   const badClick = () => {
+    setValues(feedbackValues.concat(-1))
     const updatedBad = bad + 1
     setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+  }
+
+  const getAverage = ({array, total}) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / total;
   }
 
   return (
@@ -37,10 +52,12 @@ const App = () => {
         <Button handleClick={neutralClick} text='neutral' />
         <Button handleClick={badClick} text='bad' />
       </div>
-      <h1>Statistics:</h1>
+      <h1>Give feedback:</h1>
       <Display counter={good} text='good' />
       <Display counter={neutral} text='neutral' />
       <Display counter={bad} text='bad' />
+      <Display counter={total} text='all' />
+      <Display counter={getAverage(feedbackValues, total)} text='average' />
     </div>
   )
 }
