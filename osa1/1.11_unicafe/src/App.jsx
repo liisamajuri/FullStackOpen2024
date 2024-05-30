27177fc8adcefb82418b02eb72ad02f363f6ad3e
Liisa Majuri <liisamajuri@kamk.fi>
2024-05-30 21:51:55 +0300

    Tehty tehtävä 1.11

---
+++ osa1/1.11_unicafe/src/App.jsx
@@ -0,0 +1,104 @@
+import { useState } from 'react'
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(1)} />
+        <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [feedbackValues, setValues] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const goodClick = () => {
+    setValues(feedbackValues.concat(1))
+    console.log(feedbackValues)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+
+  const neutralClick = () => {
+    setValues(feedbackValues.concat(0))
+    console.log(feedbackValues)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+  }
+
+  const badClick = () => {
+    setValues(feedbackValues.concat(-1))
+    console.log(feedbackValues)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+  }
+
+  const getAverage = ({ array, total }) => {
+    if (total === 0) return 0
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    return sum / total
+  }
+
+  const average = getAverage({ array: feedbackValues, total })
+  const positive = total === 0 ? 0 : (good / total) * 100
+
+
+  return (
+    <div>
+      <h1>Give feedback:</h1>
+      <div>
+        <Button handleClick={goodClick} text='good' />
+        <Button handleClick={neutralClick} text='neutral' />
+        <Button handleClick={badClick} text='bad' />
+      </div>
+      <h1>Statistics:</h1>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  )
+}
+
+export default App
