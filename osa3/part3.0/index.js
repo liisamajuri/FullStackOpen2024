005950c8472cd52eee49e6676fbb1fe762e4c897
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-11 20:32:08 +0300

    Korjattu luentoharjoitusta part 3.0

--- osa3/part3.0/index.js
+++ osa3/part3.0/index.js
@@ -21,6 +21,22 @@ let notes = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -80,22 +96,6 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
