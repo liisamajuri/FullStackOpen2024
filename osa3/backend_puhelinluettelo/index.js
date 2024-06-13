32deef2e1dbb05299be07b421f78a0c5ed30ef21
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-13 22:14:31 +0300

    Päivitys

--- osa3/backend_puhelinluettelo/index.js
+++ osa3/backend_puhelinluettelo/index.js
@@ -1,8 +1,10 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 
 morgan.token('body', (req) => {
