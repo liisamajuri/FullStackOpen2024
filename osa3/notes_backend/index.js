f383eb8d3609fd6fe7fd909573ad24559996b02d
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-15 21:18:47 +0300

    Jatkettu notes backendiä

--- osa3/notes_backend/index.js
+++ osa3/notes_backend/index.js
@@ -1,27 +1,12 @@
 const cors = require('cors')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -39,11 +24,13 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Welcome to Notes App!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
