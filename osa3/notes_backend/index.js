72ccd8bb6b0fa58fb0b558eb3e9e5327c5a62ef9
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-16 17:59:57 +0300

    Jatkettu notes-luentoharjoitusta

--- osa3/notes_backend/index.js
+++ osa3/notes_backend/index.js
@@ -7,6 +7,7 @@ const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
 
 const requestLogger = (request, response, next) => {
@@ -17,12 +18,10 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
+
 app.use(requestLogger)
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Welcome to Notes App!</h1>')
@@ -34,33 +33,42 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
- /* 
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
-  })
-  console.log(note)
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
+  const note = {
+    content: body.content,
+    important: body.important,
   }
-})
-*/
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
 
 /*
 const generateId = () => {
@@ -90,7 +98,23 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
