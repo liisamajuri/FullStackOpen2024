7bd9e49fdd32e8ca4edd374182babc400439205c
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-19 17:48:26 +0300

    Tehty tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  const totalSum = blogs.reduce(
+    (sum, item) => sum + item.likes, 0,
+  );
+  return blogs.length === 0
+    ? 0 
+    : totalSum
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
