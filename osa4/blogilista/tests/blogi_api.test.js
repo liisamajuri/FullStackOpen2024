0e3c98075862229c6c69a33a224fe538bf0bba56
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-21 15:54:34 +0300

    Aloitettu tehtävää 4.9

--- osa4/blogilista/tests/blogi_api.test.js
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -2,9 +2,15 @@ const { test, after, beforeEach, describe } = require('node:test')
 const Blog = require('../models/blog')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 
 const app = require('../app')
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
 
 describe('checking the correct format of the data', () => {
   test('blogs are returned as json', async () => {
