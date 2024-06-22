f4d30324aedc13da1eac894caeba04f78ce6ede7
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-22 16:14:57 +0300

    Tehty tehtävä 4.13

--- osa4/blogilista/tests/blogi_api.test.js
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -118,6 +118,24 @@ describe('addition of a new blog', () => {
 
 })
 
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+    const contents = blogsAtEnd.map(r => r.title)
+    assert(!contents.includes(blogToDelete.title))
+  })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
