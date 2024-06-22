0e5382654e7120a50288f6abfa5d784ef2c6769c
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-22 15:03:52 +0300

    Tehty tehtävä 4.9

--- osa4/blogilista/tests/blogi_api.test.js
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -3,6 +3,7 @@ const Blog = require('../models/blog')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('./test_helper')
+const assert = require('assert')
 
 const app = require('../app')
 const api = supertest(app)
@@ -18,6 +19,16 @@ describe('checking the correct format of the data', () => {
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+  })
+
+  test('returned blogs have an id field', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    blogs.forEach(blog => {
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
+    })
   })
 
   after(async () => {
