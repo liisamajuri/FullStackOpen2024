30a68f61404b7353717ab244a0b137fb64b2eba8
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-23 22:12:12 +0300

    Jatkettu Notes-harjoituksia

--- osa4/notes/controllers/notes.js
+++ osa4/notes/controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -18,29 +19,33 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
-notesRouter.post('/', async (request, response) => {
-  try {
-    const body = request.body
-    const user = await User.findById(body.userId)
-
-    if (!body.content) {
-      return response.status(400).json({ error: 'content missing' })
-    }
-
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      user: user._id
-    })
-
-    const savedNote = await note.save()
-    user.notes = user.notes.concat(savedNote._id)
-    await user.save()
-
-    response.status(201).json(savedNote)
-  } catch (exception) {
-    next(exception)
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
   }
+  return null
+}
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const note = new Note({
+    content: body.content,
+    important: body.important === undefined ? false : body.important,
+    user: user._id
+  })
+
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.json(savedNote)
 })
 
 
