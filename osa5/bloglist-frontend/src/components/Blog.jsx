ae8363c2167dc7ec212f47a807fbd9e20a2546e9
Liisa Majuri <liisamajuri@kamk.fi>
2024-06-30 21:03:24 +0300

    Tehty tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
