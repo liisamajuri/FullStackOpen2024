6ce31145eade9b5adaf47fab0be2eb3afd01610c
Liisa Majuri <liisamajuri@kamk.fi>
2024-07-05 22:34:47 +0300

    Tehty tehtävä 5.17

---
+++ osa5/blogilista_E2E/tests/blog_app.spec.js
@@ -0,0 +1,20 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+  })
+})
