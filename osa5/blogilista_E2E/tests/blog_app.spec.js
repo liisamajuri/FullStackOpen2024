f93dce51146692a185fc66d1694502bf2409d8d4
Liisa Majuri <liisamajuri@kamk.fi>
2024-07-06 17:28:40 +0300

    Tehty tehtävä 5.18

--- osa5/blogilista_E2E/tests/blog_app.spec.js
+++ osa5/blogilista_E2E/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -17,4 +18,20 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('Log in to application')).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'salainen')
+      await expect(page.getByText('Matti Luukkainen (mluukkai) logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'wrong')
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('Wrong credentials')
+
+      await expect(page.getByText('Matti Luukkainen (mluukkai) logged in')).not.toBeVisible()
+    })
+  })
+
 })
--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -154,6 +154,7 @@ const App = () => {
       <div>
         Username:
         <input
+          data-testid='username'
           type="text"
           value={username}
           name="Username"
@@ -163,6 +164,7 @@ const App = () => {
       <div>
         Password:
         <input
+          data-testid='password'
           type="password"
           value={password}
           name="Password"
