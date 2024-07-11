e71cf3a98bd644705471e31f25c3d4cd731e4df5
Liisa Majuri <liisamajuri@kamk.fi>
2024-07-11 22:09:51 +0300

    Tehty tehtävä 6.10

--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -2,7 +2,9 @@ import ReactDOM from 'react-dom/client'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
+import store from './store'
 
+/*
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
@@ -12,6 +14,7 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer)
+*/
 
 console.log(store.getState())
 
