6438371c28cc783a8dd41575e0db2782ad2c8bd9
Liisa Majuri <liisamajuri@kamk.fi>
2024-07-12 22:12:55 +0300

    Jatkettu osan 6c notes-harjoituksia

--- osa6/redux_notes/src/main.jsx
+++ osa6/redux_notes/src/main.jsx
@@ -1,22 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-//import { createStore, combineReducers } from 'redux'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux' 
+import { Provider } from 'react-redux'
+import store from './store' 
 import App from './App'
-
-import noteReducer from './reducers/noteReducer'
-
-import filterReducer from './reducers/filterReducer'
-
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
 
 console.log(store.getState())
 
@@ -24,5 +9,6 @@ console.log(store.getState())
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 )
