7f3681e3542066a879fbf17ca594f2e54ba75acf
Liisa Majuri <liisamajuri@kamk.fi>
2024-07-13 16:06:45 +0300

    Tehty tehtävä 6.17

--- osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotificationWithTimeout } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
+//import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -11,8 +11,8 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
+    //const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(content))
     dispatch(setNotificationWithTimeout(`you created a new anecdote: '${content}'`, 5000))
   }
 
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -29,9 +29,11 @@ const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState,
   reducers: {
+    /*
     createAnecdote(state, action) {
       state.push(action.payload)
     },
+    */
 
     voteAnecdote(state, action) {
       const id = action.payload
@@ -55,12 +57,19 @@ const anecdoteSlice = createSlice({
   }
 })
 
-export const { createAnecdote, voteAnecdote, appendAnecdote, setAnecdote } = anecdoteSlice.actions
+export const { voteAnecdote, appendAnecdote, setAnecdote } = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
   return async dispatch => {
     const anecdotes = await anecdoteService.getAll()
     dispatch(setAnecdote(anecdotes))
+  }
+}
+
+export const createAnecdote = content => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
   }
 }
 
