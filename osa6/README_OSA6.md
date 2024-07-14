c1fcc83700381a2d7a8a0416f482a92e98c87013
Liisa Majuri <liisamajuri@kamk.fi>
2024-07-14 20:39:55 +0300

    Tehty tehtävä 6.20

--- osa6/README_OSA6.md
+++ osa6/README_OSA6.md
@@ -7,6 +7,7 @@
 
 - Tehtäväkokonaisuudet löytyvät seuraavista hakemistoista:
   - Tehtävät 6.1-6.2: _**osa6/unicafe-redux**_
-  - Tehtävät 6.3-6.24: _**osa6/redux-anecdotes**_
+  - Tehtävät 6.3-6.19: _**osa6/redux-anecdotes**_
+  - Tehtävät 6.20-6.24: _**osa6/query-anecdotes**_
 
 
---
+++ osa6/query-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-constant-condition */
+const Notification = () => {
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  if (true) return null
+
+  return (
+    <div style={style}>
+      
+    </div>
+  )
+}
+
+export default Notification
